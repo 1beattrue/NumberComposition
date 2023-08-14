64498315a110281e076ec3cf643c540482fd49df
Vitaly <onebeattrue@gmail.com>
2023-08-14 21:28:34 +0300

    Добавление Data Binding на фрагмент GameFragment

--- app/src/main/java/edu/mirea/onebeattrue/numbercomposition/presentation/adapters/BindingAdapters.kt
+++ app/src/main/java/edu/mirea/onebeattrue/numbercomposition/presentation/adapters/BindingAdapters.kt
@@ -1,13 +1,22 @@
 package edu.mirea.onebeattrue.numbercomposition.presentation.adapters
 
-import android.health.connect.datatypes.units.Percentage
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import edu.mirea.onebeattrue.numbercomposition.R
 import edu.mirea.onebeattrue.numbercomposition.domain.entity.GameResult
-import java.lang.Exception
 
+interface ClickListener {
+    // этот интерфейс нужен, потому что если в BindingAdapter закинуть lambda,
+    // то будет преобразование (Object) -> ...
+    operator fun invoke(option: Int)
+}
+
+/** fragment_game_finished */
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
     textView.text = String.format(
@@ -57,4 +66,46 @@ private fun getSmileResId(winner: Boolean): Int {
         R.drawable.ic_smile
     else
         R.drawable.ic_sad
+}
+
+
+/** fragment_game */
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enoughCount: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enoughCount))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enoughPercent: Boolean) {
+    progressBar.progressTintList = ColorStateList.valueOf(
+        getColorByState(progressBar.context, enoughPercent)
+    )
+}
+
+@BindingAdapter("progress")
+fun bindProgress(progressBar: ProgressBar, percentOfRightAnswers: Int) {
+    progressBar.setProgress(percentOfRightAnswers, true)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState)
+        android.R.color.holo_green_light
+    else
+        android.R.color.holo_red_light
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("minPercent")
+fun bindMinPercent(progressBar: ProgressBar, minPercent: Int) {
+    progressBar.secondaryProgress = minPercent
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: ClickListener) {
+    textView.setOnClickListener { clickListener(textView.text.toString().toInt()) }
 }
